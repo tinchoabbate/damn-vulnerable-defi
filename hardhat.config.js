37de96747344c0efd9eaa89b54ebc2129182d0aa
franz.media <fkrekeler@gmail.com>
2022-03-12 21:49:04 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require('@openzeppelin/hardhat-upgrades');
 require('hardhat-dependency-compiler');
+require('dotenv').config()
 
 module.exports = {
     networks: {
