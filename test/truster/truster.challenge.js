50d42f826f9af868f4be7c0d803b302852cb9554
franz.media <fkrekeler@gmail.com>
2022-03-26 22:29:15 +0300

    bbb8

--- test/truster/truster.challenge.js
+++ test/truster/truster.challenge.js
@@ -38,10 +38,10 @@ describe('[Challenge] Truster', function () {
         // console.log(blockAfter)
         const res = await exploit.transferMe()
         // console.log("hmmmmmm, ",res)
-        const attackerTokenBalance = await this.token.allowanceOf(attacker.address)
-       
+        const allowance = await this.token.allowance(attacker.address)
+        console.log(allowance)
         const attackerTokenBalance = await this.token.balanceOf(attacker.address)
-        // console.log("WWWWWWWWWWWWWWWWW", attackerTokenBalance)
+        console.log("WWWWWWWWWWWWWWWWW", attackerTokenBalance)
         // await exploit.mint()
     });
 
