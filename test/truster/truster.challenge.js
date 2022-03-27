2a57eb9c9d892fb1b7dbf911c41b7cba5c7edec0
franz.media <fkrekeler@gmail.com>
2022-03-27 06:29:28 +0300

    debugging allowance

--- test/truster/truster.challenge.js
+++ test/truster/truster.challenge.js
@@ -31,15 +31,15 @@ describe('[Challenge] Truster', function () {
         /** CODE YOUR EXPLOIT HERE     */
         const TrusterExploitFactory = await ethers.getContractFactory('TrusterAttack', attacker);
  
-        // const blockBefore = await ethers.provider.getBlockNumber();
-        // console.log(blockBefore)
+        const blockBefore = await ethers.provider.getBlockNumber();
+        console.log("Starting exploit on block:", blockBefore)
         const exploit = await TrusterExploitFactory.deploy(this.pool.address, this.token.address, attacker.address)
-        // const blockAfter = await ethers.provider.getBlockNumber( );
-        // console.log(blockAfter)
-        const res = await exploit.transferMe()
-        // console.log("hmmmmmm, ",res)
+        const blockAfter = await ethers.provider.getBlockNumber( );
+        console.log("Starting exploit on block:", blockBefore)
         const allowance = await this.token.allowance(attacker.address)
         console.log(allowance)
+        const res = await exploit.transferMe()
+        // console.log("hmmmmmm, ",res)
         const attackerTokenBalance = await this.token.balanceOf(attacker.address)
         console.log("WWWWWWWWWWWWWWWWW", attackerTokenBalance)
         // await exploit.mint()
