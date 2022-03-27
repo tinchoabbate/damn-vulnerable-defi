2d3ff3bed0babc38385970ec441e30d3b079910e
franz.media <fkrekeler@gmail.com>
2022-03-27 12:31:31 +0300

    .

--- test/the-rewarder/the-rewarder.challenge.js
+++ test/the-rewarder/the-rewarder.challenge.js
@@ -95,9 +95,9 @@ describe('[Challenge] The rewarder', function () {
         }
         
         // Rewards must have been issued to the attacker account
-        console.log("@1 reward token totalsupply = ", await this.rewardToken.totalSupply());
+        console.log("@1 reward token totalsupply = ", parseInt(await this.rewardToken.totalSupply(),16));
         expect(await this.rewardToken.totalSupply()).to.be.gt(ethers.utils.parseEther('100'));
-        console.log("@2 reward token totalsupply = ", await this.rewardToken.totalSupply());
+        console.log("@2 reward token totalsupply = ", parseInt(await this.rewardToken.totalSupply(),16));
         let rewards = await this.rewardToken.balanceOf(attacker.address);
 
         // The amount of rewards earned should be really close to 100 tokens
