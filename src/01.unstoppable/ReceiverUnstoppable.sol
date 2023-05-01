b2360147c7df88575941a0c2f7d46e34befe9374
Erik <esnetto@gmail.com>
2023-05-01 14:59:20 +0000

    move from node_modules to submodules part 1

--- src/01.unstoppable/ReceiverUnstoppable.sol
+++ src/01.unstoppable/ReceiverUnstoppable.sol
@@ -3,36 +3,42 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
 import "solmate/src/auth/Owned.sol";
-import {UnstoppableVault, ERC20} from "./UnstoppableVault.sol";
+import { UnstoppableVault, ERC20 } from "./UnstoppableVault.sol";
 
 /**
  * @title ReceiverUnstoppable
  * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
  */
 contract ReceiverUnstoppable is Owned, IERC3156FlashBorrower {
-    UnstoppableVault private immutable pool;
+  UnstoppableVault private immutable pool;
 
-    error UnexpectedFlashLoan();
+  error UnexpectedFlashLoan();
 
-    constructor(address poolAddress) Owned(msg.sender) {
-        pool = UnstoppableVault(poolAddress);
+  constructor(address poolAddress) Owned(msg.sender) {
+    pool = UnstoppableVault(poolAddress);
+  }
+
+  function onFlashLoan(
+    address initiator,
+    address token,
+    uint256 amount,
+    uint256 fee,
+    bytes calldata
+  ) external returns (bytes32) {
+    if (
+      initiator != address(this) || msg.sender != address(pool)
+        || token != address(pool.asset()) || fee != 0
+    ) {
+      revert UnexpectedFlashLoan();
     }
 
-    function onFlashLoan(address initiator, address token, uint256 amount, uint256 fee, bytes calldata)
-        external
-        returns (bytes32)
-    {
-        if (initiator != address(this) || msg.sender != address(pool) || token != address(pool.asset()) || fee != 0) {
-            revert UnexpectedFlashLoan();
-        }
+    ERC20(token).approve(address(pool), amount);
 
-        ERC20(token).approve(address(pool), amount);
+    return keccak256("IERC3156FlashBorrower.onFlashLoan");
+  }
 
-        return keccak256("IERC3156FlashBorrower.onFlashLoan");
-    }
-
-    function executeFlashLoan(uint256 amount) external onlyOwner {
-        address asset = address(pool.asset());
-        pool.flashLoan(this, asset, amount, bytes(""));
-    }
+  function executeFlashLoan(uint256 amount) external onlyOwner {
+    address asset = address(pool.asset());
+    pool.flashLoan(this, asset, amount, bytes(""));
+  }
 }
--- test/01.unstoppable/unstoppable.t.sol
+++ test/01.unstoppable/unstoppable.t.sol
@@ -8,55 +8,56 @@ import "src/01.unstoppable/ReceiverUnstoppable.sol";
 import "src/DamnValuableToken.sol";
 
 contract ChallengeUnstoppable is Test {
-    DamnValuableToken public token;
-    UnstoppableVault public vault;
-    ReceiverUnstoppable public receiver;
-    uint256 public constant TOKENS_IN_VAULT = 1_000_000 ether;
-    uint256 public constant INITIAL_PLAYER_TOKEN_BALANCE = 10 ether;
+  DamnValuableToken public token;
+  UnstoppableVault public vault;
+  ReceiverUnstoppable public receiver;
+  uint256 public constant TOKENS_IN_VAULT = 1_000_000 ether;
+  uint256 public constant INITIAL_PLAYER_TOKEN_BALANCE = 10 ether;
 
-    address public deployer = makeAddr("deployer");
-    address public player = makeAddr("player");
-    address public someUser = makeAddr("someUser");
+  address public deployer = makeAddr("deployer");
+  address public player = makeAddr("player");
+  address public someUser = makeAddr("someUser");
 
-    function setUp() public {
-        /**
-         * SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE
-         */
-        changePrank(deployer);
-        token = new DamnValuableToken();
-        vault = new UnstoppableVault(token, deployer, deployer);
+  function setUp() public {
+    /**
+     * SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE
+     */
+    changePrank(deployer);
+    token = new DamnValuableToken();
+    vault = new UnstoppableVault(token, deployer, deployer);
 
-        assertEq(address(vault.asset()), address(token));
+    assertEq(address(vault.asset()), address(token));
 
-        token.approve(address(vault), TOKENS_IN_VAULT);
-        vault.deposit(TOKENS_IN_VAULT, deployer);
+    token.approve(address(vault), TOKENS_IN_VAULT);
+    vault.deposit(TOKENS_IN_VAULT, deployer);
 
-        assertEq(token.balanceOf(address(vault)), TOKENS_IN_VAULT);
-        assertEq(vault.totalAssets(), TOKENS_IN_VAULT);
-        assertEq(vault.totalSupply(), TOKENS_IN_VAULT);
-        assertEq(vault.maxFlashLoan(address(token)), TOKENS_IN_VAULT);
-        assertEq(vault.flashFee(address(token), TOKENS_IN_VAULT - 1), 0);
-        assertEq(vault.flashFee(address(token), TOKENS_IN_VAULT), 50000 ether);
+    assertEq(token.balanceOf(address(vault)), TOKENS_IN_VAULT);
+    assertEq(vault.totalAssets(), TOKENS_IN_VAULT);
+    assertEq(vault.totalSupply(), TOKENS_IN_VAULT);
+    assertEq(vault.maxFlashLoan(address(token)), TOKENS_IN_VAULT);
+    assertEq(vault.flashFee(address(token), TOKENS_IN_VAULT - 1), 0);
+    assertEq(vault.flashFee(address(token), TOKENS_IN_VAULT), 50000 ether);
 
-        token.transfer(player, INITIAL_PLAYER_TOKEN_BALANCE);
-        assertEq(token.balanceOf(player), INITIAL_PLAYER_TOKEN_BALANCE);
+    token.transfer(player, INITIAL_PLAYER_TOKEN_BALANCE);
+    assertEq(token.balanceOf(player), INITIAL_PLAYER_TOKEN_BALANCE);
 
-        // Show it's possible for someUser to take out a flash loan
-        changePrank(someUser);
-        receiver = new ReceiverUnstoppable(address(vault));
-        receiver.executeFlashLoan(100 ether);
-    }
+    // Show it's possible for someUser to take out a flash loan
+    changePrank(someUser);
+    receiver = new ReceiverUnstoppable(address(vault));
+    receiver.executeFlashLoan(100 ether);
+  }
 
-    function test_solution() public {
-        /**
-         * CODE YOUR SOLUTION HERE
-         */
-        changePrank(player);
-        /**
-         * SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE
-         */
-        vm.expectRevert();
-        receiver.executeFlashLoan(100 ether);
-        // It is no longer possible to execute flash loans
-    }
+  function test_solution() public {
+    /**
+     * CODE YOUR SOLUTION HERE
+     */
+    changePrank(player);
+    /**
+     * SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE
+     */
+    changePrank(someUser);
+    vm.expectRevert();
+    receiver.executeFlashLoan(100 ether);
+    // It is no longer possible to execute flash loans
+  }
 }
